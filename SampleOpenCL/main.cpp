fd3619755f33136ffc582350a76fd20b6edd40f9
Rajkumar Mohan <rajkumarcm@yahoo.com>
2018-03-15 22:25:23 +0530

    Initial Commit

---
+++ SampleOpenCL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SampleOpenCL
+//
+//  Created by Rajkumar Conjeevaram Mohan on 15/03/18.
+//  Copyright © 2018 Rajkumar Conjeevaram Mohan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
